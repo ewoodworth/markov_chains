50094730de2f08ace4d2cb68b2c17af151d1efb1
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-04 12:01:45 -0700

    first commit

---
+++ markov.py
@@ -0,0 +1,69 @@
+from random import choice
+
+
+def open_and_read_file(file_path):
+    """Takes file path as string; returns text as string.
+
+    Takes a string that is a file path, opens the file, and turns
+    the file's contents as one string of text.
+    """
+
+    tex_file = open(file_path)
+    full_text = tex_file.read()
+    full_text_stripped = full_text.rstrip()
+
+    return full_text_stripped
+
+
+def make_chains(text_string):
+    """Takes input text as string; returns _dictionary_ of markov chains.
+
+    A chain will be a key that consists of a tuple of (word1, word2)
+    and the value would be a list of the word(s) that follow those two
+    words in the input text.
+
+    For example:
+
+        >>> make_chains("hi there mary hi there juanita")
+        {('hi', 'there'): ['mary', 'juanita'], ('there', 'mary'): ['hi'], ('mary', 'hi': ['there']}
+    """
+
+    chains = {}
+
+    words = text_string.split(" ")
+    
+    for i in range(len(words)):
+        if words[i] == words[-1]:
+            print words[i]
+        else:
+            print words[i], words[i + 1]
+
+    return chains
+
+
+def make_text(chains):
+    """Takes dictionary of markov chains; returns random text."""
+
+    text = ""
+
+    # your code goes here
+
+    return text
+
+
+input_path = "green-eggs.txt"
+
+processed_file = open_and_read_file(input_path)
+
+make_chains(processed_file)
+
+# # Open the file and turn it into one long string
+# input_text = open_and_read_file(input_path)
+
+# # Get a Markov chain
+# chains = make_chains(input_text)
+
+# # Produce random text
+# random_text = make_text(chains)
+
+# print random_text
